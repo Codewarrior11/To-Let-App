efed28325b5b7b958259974f9b789c72ef94bb41
Appel mahmud akib <Appelmahmud74@gmail.com>
2018-09-21 21:56:09 +0600

    FireBase Test

--- app/src/main/java/com/infinity/tolet/appel/to_let/RentView.java
+++ app/src/main/java/com/infinity/tolet/appel/to_let/RentView.java
@@ -1,13 +1,42 @@
 package com.infinity.tolet.appel.to_let;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class RentView extends AppCompatActivity {
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRef = database.getReference("addPost/p1/owner");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rent_view);
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String value=dataSnapshot.getValue(String.class);
+                Toast.makeText(RentView.this,value,Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 }
